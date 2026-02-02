e49f9104adf51d64b5862ceeeca58a29abe4ac68
Atia8 <zamanatia8@gmail.com>
2026-02-03 00:28:29 +0600

    helper verification done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,4 +96,19 @@ dependencies {
 
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // CameraX
+    implementation("androidx.camera:camera-core:1.3.0")
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
+// ML Kit Face Detection
+    implementation("com.google.mlkit:face-detection:16.1.5")
+    implementation("com.google.mlkit:text-recognition:16.0.0")
+// Jetpack Compose
+    implementation("androidx.compose.ui:ui:1.5.0")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
 }
